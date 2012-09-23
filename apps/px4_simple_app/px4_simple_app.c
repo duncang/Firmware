047a9861dd106cd6b1a569bbd3c0dcde7e56fc8a
Duncan Greer <duncangreer@gmail.com>
2012-09-23 16:11:57 +1000

    Updated example to read orb data

--- apps/px4_simple_app/px4_simple_app.c
+++ apps/px4_simple_app/px4_simple_app.c
@@ -40,12 +40,64 @@
 #include <nuttx/config.h>
 #include <stdio.h>
 #include <errno.h>
+
+// use poll() for blocking wait on the orb fd. note poll() vs select(): http://www.unixguide.net/network/socketfaq/2.14.shtml
+#include <poll.h>
+
+/* orb topics */
+#include <uORB/topics/sensor_combined.h>
  
 __EXPORT int px4_simple_app_main(int argc, char *argv[]);
- 
+
+
+
+
+
 int px4_simple_app_main(int argc, char *argv[])
 {
-	printf("Hello Sky!\n");
+	printf("Starting Simple PX4 App!\n");
+
+	// subscribe to sensor_combined topic
+	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
+
+	struct pollfd fds[] = {
+		{ .fd = sensor_sub_fd, .events = POLLIN },
+
+	};
+
+	int error_counter = 0;
+
+	while(true)
+	{
+		
+		int poll_ret = poll(fds,1,1000);  // wait for sensor update of 1 file descriptor for 1000ms
+
+		if (poll_ret == 0)
+		{
+			printf("[px4_simple_app] Got no data within 1 sec\n");
+		} else if (poll_ret < 0)
+		{
+			if (error_counter < 10 || error_counter % 50 == 0)
+			{
+				printf("[px4_simple_app] ERROR: return value from poll(): %d\n", poll_ret);
+			}
+			++error_counter;
+		} else
+		{
+			// retrieve the data
+
+			
+			if (fds[0].revents & POLLIN)
+			{
+				struct sensor_combined_s raw;
+				orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+				printf("Accelerometer:\t%8.4lf\t%8.4lf\t%8.4lf\n",  
+					(double)raw.accelerometer_m_s2[0],
+					(double)raw.accelerometer_m_s2[1],
+					(double)raw.accelerometer_m_s2[2]);
+			}
+		}
+	}
 	return OK;
 }
 
